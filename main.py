6587bb613fb79cfebdbaca6a1d7e65d3d17a95e8
Tung Do <tdo@haverford.edu>
2020-11-08 19:40:38 -0500

    create a chat room at /chat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, session
 from werkzeug.security import generate_password_hash, check_password_hash
 import pyrebase
+from flask_socketio import SocketIO, send
 from database.firebase_config import firebase_config
 
 firebase = pyrebase.initialize_app(firebase_config)
@@ -9,9 +10,18 @@ db = firebase.database()
 
 app = Flask(__name__)
 
+socketio=SocketIO(app)
+
 @app.route('/')
 def homepage():
-    return 'Homepage'
+    return render_template('home.html')
+
+@app.route('/chat')
+def chat(): 
+    return render_template("chat.html")
+@socketio.on('message')
+def handle_message(msg): 
+    send(msg,broadcast=True)
 
 @app.route('/register', methods = ["GET", "POST"])
 def register():
@@ -31,4 +41,5 @@ def register():
         return render_template("register.html")
 
 if __name__ == "__main__":
-    app.run()
+    #app.run()
+    socketio.run(app)
