b5f50d7994683693a6ccdca3119561169d2f8eb9
Tung Do <tdo@haverford.edu>
2020-11-09 01:14:17 -0500

    reimplement exit fucntion of chatroom

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 import pyrebase
 from flask_socketio import SocketIO, send
 from database.firebase_config import firebase_config
-
+import json 
 firebase = pyrebase.initialize_app(firebase_config)
 
 db = firebase.database()
@@ -12,12 +12,16 @@ app = Flask(__name__)
 
 socketio=SocketIO(app)
 
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def homepage():
-    return render_template('home.html')
+    data=session['data']
+    session['data']=''
+    return render_template('home.html', flash=data)
 
-@app.route('/chat')
+@app.route('/chat', methods = ["GET", "POST"])
 def chat(): 
+    if request.method == "POST": 
+        session['data']='You are unmatched'
     return render_template("chat.html")
 @socketio.on('message')
 def handle_message(msg): 
@@ -41,4 +45,5 @@ def register():
         return render_template("register.html")
 
 if __name__ == "__main__":
+    app.secret_key = '123456789'
     socketio.run(app)
