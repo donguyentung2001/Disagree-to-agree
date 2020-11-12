0d7d1243477593d3ae39496389f8b26070120f90
ltta18 <thuylinh7720@gmail.com>
2020-11-13 00:54:59 +0700

    Add Matching

---
+++ frontend/src/pages/Matching/index.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+import { Button } from 'antd';
+import React, { useState } from 'react';
+import './index.scss';
+
+const Matching = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <>
+      {isLoading
+        ? (
+          <div>
+            <h1>Wait a moment...</h1>
+            <h3>We are finding a match for you</h3>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+              className="avatar"
+              alt="avatar"
+            />
+            <Button size="large" className="black-box">Cancel Matching</Button>
+          </div>
+        )
+        : (
+          <div>
+            <h1>You got a match!</h1>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg"
+              className="avatar"
+              alt="avatar"
+            />
+            <Button size="large" className="black-box">Join Conversation</Button>
+          </div>
+        )}
+    </>
+  );
+};
+
+export default Matching;
