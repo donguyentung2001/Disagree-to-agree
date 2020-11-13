5b9bb199457e5fdc63c6951a6181e243420403a1
samueltan97 <stan1@haverford.edu>
2020-11-13 02:22:53 +0000

    Running site before merging tung's new chat feature

--- backend/profile_analysis/sentiment_analysis.py
+++ backend/profile_analysis/sentiment_analysis.py
@@ -1,3 +1,4 @@
+from apiclient.discovery import build
 from google.cloud import language_v1
 import numpy as np
 from textblob import TextBlob
@@ -14,24 +15,26 @@ def analyze_google_sentiment(text_content):
     """
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(os.getcwd(), 'profile_analysis','yhack-55a358192c91.json')
 
-    client = language_v1.LanguageServiceClient()
+    
 
-    type_ = language_v1.Document.Type.PLAIN_TEXT
+    service = build('language', 'v1', developerKey='AIzaSyBkpse_KLTykTiGr5r3Oo_RMu939EuQX9Q')                
+    collection = service.documents()
 
-    language = "en"
-    document = {"content": text_content, "type_": type_, "language": language}
+    # language = "en"
+    # document = {"content": text_content, "type_": type_, "language": language}
 
-    encoding_type = language_v1.EncodingType.UTF8
+    # encoding_type = language_v1.EncodingType.UTF8
+    # response = client.analyze_sentiment(request = {'document': document, 'encoding_type': encoding_type})
+    
+    data = {}
+    data['document'] = {}
+    data['document']['language'] = 'en'
+    data['document']['content'] = text_content
+    data['document']['type'] = 'PLAIN_TEXT'
 
-    response = client.analyze_sentiment(request = {'document': document, 'encoding_type': encoding_type})
-    # Get overall sentiment of the input document
-    # print(u"Document sentiment score: {}".format(response.document_sentiment.score))
-    # print(
-    #     u"Document sentiment magnitude: {}".format(
-    #         response.document_sentiment.magnitude
-    #     )
-    # )
-    return response.document_sentiment.score
+    request = collection.analyzeSentiment(body=data)
+    res = request.execute()
+    return res['documentSentiment']['score']
 
 
 def find_sentiments(cleaned_keywords):
