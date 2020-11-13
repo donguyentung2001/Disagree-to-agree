f31490a2fc5f6428c052d12d9670b7fc780d1cc9
ltta18 <thuylinh7720@gmail.com>
2020-11-13 16:54:32 +0700

    Add api utils

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,20 @@
+import Axios from 'axios';
+
+export default {
+  checkLoggedIn: (history, pathname) => {
+    Axios.get('http://localhost:5000/loggedin',
+      {
+        headers: { 'Access-Control-Allow-Origin': '*' },
+      })
+      .then((res) => {
+        const { data } = res;
+        if (data.logged_in) {
+          localStorage.setItem('user', JSON.stringify(data));
+          if (['/signin', '/signup'].includes(pathname)) history.push('/');
+        }
+        localStorage.setItem('user', JSON.stringify({}));
+        if (!(['/signin', '/signup'].includes(pathname))) history.push('/signin');
+      })
+      .catch((error) => console.log(error));
+  },
+};
